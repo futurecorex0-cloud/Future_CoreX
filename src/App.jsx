18c881d434fd223d8f20d23086447355f0753559
SHAHZAIB <futurecorex0@GMAIL.COM>
2025-10-30 12:30:44 +0500

    First Change

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import Contact from "./Screens/Contact";
 export default function App() {
   const [activeScreen, setActiveScreen] = useState("Home");
 
-  // ✅ Screen Renderer
   const renderScreen = () => {
     const screens = {
       Home: <Home setActiveScreen={setActiveScreen} />,
@@ -31,7 +30,7 @@ export default function App() {
 
     const toggle = document.querySelector(".navbar-toggler");
     if (toggle && window.getComputedStyle(toggle).display !== "none") {
-      toggle.click(); // closes the collapse menu
+      toggle.click();
     }
   };
 
